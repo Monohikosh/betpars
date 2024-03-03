5763c92472744762122534db60782369dc56fb77
Monohikosh <zemlyakov02@icloud.com>
2024-03-03 13:32:59 +0300

    create project

---
+++ src/main/java/ru/project/betpars/BetparsApplication.java
@@ -0,0 +1,13 @@
+package ru.project.betpars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetparsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetparsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/project/betpars/BetparsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.project.betpars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetparsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
